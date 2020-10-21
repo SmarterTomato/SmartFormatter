ee084a98dd4833bbc1fbcc0f289169147aeb6408
Tommy <tzhao@straxcorp.com>
2020-10-21 12:23:58 +1100

    Bug fixes with merge line due to encoding issue

--- src/Models/Constants.ts
+++ src/Models/Constants.ts
@@ -1,5 +1,5 @@
 // SmartFormatter is two word
-export const GET_EACH_WORD_REGEX = /[A-Z][a-z]+|[a-z]+|[A-Z]+/g;
+export const GET_EACH_WORD_REGEX = /[A-Z0-9][a-z0-9]+|[a-z0-9]+|[A-Z0-9]+/g;
 
 // SmartFormatter is one word
 export const GET_EACH_CONNECTED_STRING_REGEX = /[a-zA-Z0-9]+/g;
@@ -8,4 +8,4 @@ export const LINE_BREAK_REGEX = /.*[\r\n]+|.+$/g;
 
 export const WHITE_SPACE_REGEX = /^\s+/g;
 
-export const NEW_LINE_REGEX = /\r\n/g;
+export const NEW_LINE_REGEX = /\r\n|\n/g;
